aeceed9444b7e42c28f62b648db010fd16a34f48
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-12 13:58:17 +0400

    Js Intro

---
+++ 11.Js Intro/src/js/app.js
@@ -0,0 +1,135 @@
+// console.log("Hello Shekili")
+// console.warn("Hello Shekili")
+// console.error("Hello Shekili")
+
+
+//Variables & DataTypes
+
+
+// var let const
+//  const a = 10;
+//  a=20;
+//  console.log(a)
+
+//DataTypes
+// 1. Primitive DataTypes
+// 1.1 String
+// var ad="Natavan";
+// console.log(typeof ad)
+// 1.2 Number
+// var x =20;
+// console.log(typeof x)
+// 1.3 Boolean
+// var isMarried=true;
+// console.log(typeof isMarried)
+// 1.4 Undefined
+// var a;
+// console.log(typeof a)
+// 1.5 Null
+// var a=null;
+// console.log(typeof a)
+
+
+// 2. Non-Primitive DataTypes & Reference DataTypes
+// 2.1 Object
+
+// 2.1.1 Array
+// var nums=[1,2,3,4,5,6,7,8,9,10]
+// console.log(nums[nums.length-1])
+// 2.1.2 Class
+
+// 2.1.3 Objects
+
+// var person = {
+//     ad:"Chingiz",
+//     soyad:"Memmedov",
+//     yas:25,
+//     isMarried:false,
+//     isHappy:true,
+//     girlfriend:"JS",
+//     birthYear:function(){
+//         return new Date().getFullYear()-this.yas;
+//     }
+// }
+
+// console.log(person.birthYear())
+// let body=document.getElementById("body");
+// let lamp=document.getElementById("lamp")
+
+// function DarkMode(){
+//     // body.style.backgroundColor="black"
+//     // body.style.color="white"
+//     // lamp.style.filter="grayscale(1)"
+
+// }
+// function LightMode(){
+//     // body.style.backgroundColor="white"
+//     // body.style.color="black"
+//     // lamp.style.filter="grayscale(0)"
+
+// }
+
+
+// console.log(document.getElementById("user-name"))
+// document.getElementById("username").innerHTML="<i>Vuqar</i>";
+// document.getElementById("user-name").innerText="<i>Vuqar</i>";
+
+//Operators
+// var a=10;
+// var b="20";
+// var c="30";
+// var d="50"
+// console.log(+b+ +a+ +c+ +d)
+
+// var a=10;
+// var b="10";
+
+// if(a!==b){
+//     console.log("beraber deyil")
+// }
+// else{
+//     console.log("Beraberdir")
+// }
+
+
+
+// var x="Sekili";
+
+
+// if(x==="shekili"){
+//     console.log("sekili necesen?")
+// }
+// else{
+//     console.log("Sekili hardadi o gelsin")
+// }
+
+
+// x==="Shekili"?console.log("Sekili necesen?"):console.log("Shekili hardadi o gelsin");
+
+// console.log(new Date().getMilliseconds())
+
+// alert("Salam front2")
+// confirm()
+// prompt("Adini daxil et")
+
+
+// let time= new Date().getHours();
+// console.log(time)
+
+// if(time>=6 && time<12){
+// alert("Sabahin xeyir")
+// }
+// else if(time>=12 && time<18){
+// alert("Gunortan xeyir")
+// }
+// else if(time>=18 && time <6){
+// alert("Di get yat bala gecdi")
+// }
+
+// let h1=document.getElementById("NumText");
+
+// function Random(){
+//     h1.innerHTML+=Math.round(Math.random()*100)+" "
+// }
+
+
