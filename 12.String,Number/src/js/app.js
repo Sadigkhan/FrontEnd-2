7c84db77ea220759999e1b2ea5cef2d229157b91
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-14 12:50:59 +0400

    String,Number

---
+++ 12.String,Number/src/js/app.js
@@ -0,0 +1,78 @@
+// var text="Hello World";
+// var text2 ='hello world';
+// var text3=`hello
+// world`;
+// var text4 = new String("String object created")
+// console.log(typeof text)
+// console.log(typeof text2)
+// console.log(typeof text3)
+// console.log(typeof text4)
+// console.log(text4)
+
+// var text="Natavan Nagiyeva";
+// var anotherText="Nagiyeva";
+// console.log(text.length)
+// console.log(text.toLocaleLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.trim())
+// console.log(text.trimEnd())
+// console.log(text.trimStart())
+// console.log(text.charAt(4))
+// console.log(text.charCodeAt(4))
+// console.log(text.split(" "))
+// console.log(text.concat(" ",anotherText))
+
+// console.log(text.replace(/Firuz/g,"Shekili"))
+// console.log(text.replaceAll("Firuz","Shekili"))
+// console.log(text.substring(0,5))
+// console.log(text.slice(-5))
+// console.log(text.substr(0,2))
+
+//Numbers
+
+// var num=100000000000000000000;
+// // var num2=new Number(4.5);
+// console.log(num.toExponential())
+// console.log(num.toString().length)
+
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number("10"))
+// console.log(Number("  10"))
+// console.log(Number("10  "))
+// console.log(Number(" 10  "))
+// console.log(Number("10.33"))
+// console.log(Number("10,33"))
+// console.log(Number("10 33"))
+// console.log(Number("John"))
+
+// console.log(parseInt(true))
+// console.log(parseInt(false))
+// console.log(parseInt("10"))
+// console.log(parseInt("  10"))
+// console.log(parseInt("10  "))
+// console.log(parseInt(" 10  "))
+// console.log(parseInt("10.33"))
+// console.log(parseInt("10,33"))
+// console.log(parseInt("10 33"))
+// console.log(parseInt("John"))
+
+
+// console.log(parseFloat("-10"))
+// console.log(parseFloat("-10.33"))
+// console.log(parseFloat("10"))
+// console.log(parseFloat("  10"))
+// console.log(parseFloat("10  "))
+// console.log(parseFloat(" 10  "))
+// console.log(parseFloat("10.33"))
+// console.log(parseFloat("10,33"))
+// console.log(parseFloat("10 33"))
+// console.log(parseFloat("1.5John"))
+
+
+console.log(isNaN("5 l"))
+
+console.log()
+
+
+
