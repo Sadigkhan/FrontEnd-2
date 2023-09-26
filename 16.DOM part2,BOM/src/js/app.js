6e4331e4335d1a7307a03a1cce3637d542d98ea5
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-26 13:08:58 +0400

    Events

---
+++ 16.DOM part2,BOM/src/js/app.js
@@ -0,0 +1,72 @@
+// let menu_btn = document.querySelector("#Header .container .all .right .menu-mobile-btn");
+
+// let menu_dropdown = document.querySelector("#Header .container .all .dropdown");
+
+// menu_btn.addEventListener("click", function() {
+//     menu_dropdown.classList.toggle("active");
+// })
+
+// // menu_btn.addEventListener("click", function() {
+// //     if(menu_dropdown.classList.contains("active")){
+// //         menu_dropdown.classList.remove("active");
+// //     }else{
+// //         menu_dropdown.classList.add("active");
+// //     }
+// // })
+
+// let dropdown_items = document.querySelectorAll(".dropdown-item");
+
+// dropdown_items.forEach(element => {
+//     element.addEventListener("click", function(){
+
+//         let temp_active = document.querySelector(".dropdown-item .active")
+
+//         if(temp_active !== null){
+//             temp_active.classList.remove("active")
+//             temp_active.previousElementSibling.querySelector("i").classList.toggle("open")
+//             if(temp_active != element.querySelector(".inner-dropdown")){
+//                 element.querySelector(".dropdown-all i").classList.toggle("open")
+//                 element.querySelector(".inner-dropdown").classList.toggle("active")
+//             }
+//         }else{
+//             element.querySelector(".dropdown-all i").classList.toggle("open")
+//             element.querySelector(".inner-dropdown").classList.toggle("active")
+//         }
+
+        
+//     })
+// })
+
+
+// let first=document.getElementById("first")
+// let first2=window.document.getElementById("first")
+
+
+
+// console.log(first2)
+
+
+// let btn=document.getElementById("btn")
+
+
+// function OpenTab(){
+//     window.open("https://www.w3schools.com/js/js_window.asp")
+// }
+// window.location.assign(href)
+// window.location.href="google.com"
+// btn.addEventListener("click",OpenTab);
+
+// let btn=document.getElementById("btn")
+// let startInterval=setInterval(SayHello,2000)
+
+// function SayHello(){
+//     console.log("Salam")
+// }
+
+// function StopInterval(){
+//     clearInterval(startInterval)
+// }
+
+// // btn.addEventListener("click",StopInterval)
+
+// setTimeout(StopInterval,10000)
